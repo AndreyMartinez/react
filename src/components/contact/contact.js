7326c5bfafc99271342f26dbfb2398cfdc8677de
root <root@localhost.localdomain>
2018-08-28 17:34:18 -0400

    fORM uSER

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,25 +4,59 @@ class Contact extends Component {
     constructor() {
         super();
         this.state = {
-            name: "element"
+          email: '',
+          password: '',
+          everFocusedEmail: false,
+          everFocusedPassword: false,
+          inFocus: '',
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      
+      handleSubmit(evt) {
+        if (!this.canBeSubmitted()) {
+          evt.preventDefault();
+          return;
         }
-        this.sendForm = this.sendForm.bind(this);
-    }
-
-sendForm(){
-    console.log('entro'+ this.state.name)
-}
-    
-
-    render() {
+        const { email, password } = this.state;
+        alert(`Signed up with email: ${email} password: ${password}`);
+      }
+      
+      canBeSubmitted() {
+        const errors = this.validate(this.state.email, this.state.password);
+        const isDisabled = Object.keys(errors).some(x => errors[x]);
+        return !isDisabled;
+      }
+      validate(email, password) {
+        // true means invalid, so our conditions got reversed
+        return {
+          email: email.length === 0,
+          password: password.length === 0,
+        };
+      }
+      
+      render() {
+        const errors = this.validate(this.state.email, this.state.password);
+        const isDisabled = Object.keys(errors).some(x => errors[x]);
         return (
-            <div>
-                <input type="email" value={this.state.name} onChange={e => this.setState({
-                   name: e.target.value 
-                })}/>
-                <button onClick={this.sendForm}>Mostar</button>
-            </div>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              className={errors.email ? "error" : ""}
+              type="text"
+              placeholder="Enter email"
+              value={this.state.email}
+              onChange={e =>  this.setState({ email: e.target.value })}
+            />
+            <input
+              className={errors.password ? "error" : ""}
+              type="password"
+              placeholder="Enter password"
+              value={this.state.password}
+              onChange={ e =>   this.setState({ password: e.target.value })}
+            />
+            <button disabled={isDisabled}>Sign up</button>
+          </form>
         )
+      }
     }
-}
 export default Contact
